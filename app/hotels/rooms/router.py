d08662fbdefb1dc19428991c435e950deee7fee8
igor <frakestolstm@gmail.com>
2023-08-05 19:12:07 +0400

    deleted get_hotel_rooms and it's schema

--- app/hotels/rooms/router.py
+++ app/hotels/rooms/router.py
@@ -1,13 +1,5 @@
-from datetime import date
-
 from app.hotels.rooms.dao import RoomsDAO
-from app.hotels.rooms.schemas import HotelRoomsSchema
 from app.hotels.router import router
-
-
-@router.get("/{hotel_id}/rooms", response_model=list[HotelRoomsSchema])
-async def get_hotel_rooms(hotel_id: int, date_from: date, date_to: date):
-    return await RoomsDAO.get_hotel_rooms(hotel_id, date_from, date_to)
 
 
 @router.get("/rooms/{room_id}")
