9cb7c5cb19d31e96bda474ab12591fd514805836
igor <frakestolstm@gmail.com>
2023-08-05 23:39:27 +0400

    added http exception when there's no room

--- app/hotels/rooms/router.py
+++ app/hotels/rooms/router.py
@@ -1,7 +1,11 @@
+from app.hotels.exceptions import RecordDoesNotExists
 from app.hotels.rooms.dao import RoomsDAO
 from app.hotels.router import router
 
 
 @router.get("/rooms/{room_id}")
 async def get_room(room_id: int):
-    return await RoomsDAO.get_by_id(room_id)
+    room = await RoomsDAO.get_by_id(room_id)
+    if not room:
+        raise RecordDoesNotExists
+    return room
