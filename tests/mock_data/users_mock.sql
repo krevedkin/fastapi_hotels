6cd25f8a17d222efa4ce2b112cb8cba6430d5465
igor <frakestolstm@gmail.com>
2023-08-10 16:58:59 +0400

    added users role

--- tests/mock_data/users_mock.sql
+++ tests/mock_data/users_mock.sql
@@ -1,9 +1,11 @@
-INSERT INTO users (email, hashed_password)
+INSERT INTO users (email, hashed_password, role)
 VALUES (
     'user@example.com',
-    '$2b$12$AH9QUNC3ThhI7REDWedAq.eraz1tG7jq0H8nSQrTzt/mxyAKb.QvC'
+    '$2b$12$AH9QUNC3ThhI7REDWedAq.eraz1tG7jq0H8nSQrTzt/mxyAKb.QvC',
+    'admin'
   ),
 (
     'user2@example.com',
-    '$2b$12$AH9QUNC3ThhI7REDWedAq.eraz1tG7jq0H8nSQrTzt/mxyAKb.QvC'
+    '$2b$12$AH9QUNC3ThhI7REDWedAq.eraz1tG7jq0H8nSQrTzt/mxyAKb.QvC',
+    'user'
   );
